33467ae1c5ee42d388b70703870693e35204f2e1
NafiGit <2100032542@kluniversity.in>
2024-07-22 15:19:05 +0530

    removed proxy from client/package.json, modernized Application.js to remove p div invalid html warning, conditionally displayed department name upon dept not being null

--- client/src/components/RecentAnnouncements.js
+++ client/src/components/RecentAnnouncements.js
@@ -1,103 +1,95 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import CalendarIcon from "react-calendar-icon";
 import { ThemeProvider } from "styled-components";
 
-export default class RecentAnnouncements extends React.Component {
-  _isMounted = false;
+const RecentAnnouncements = () => {
+  const [recentAnnouncements, setRecentAnnouncements] = useState([]);
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      recentAnnouncements: [],
-    };
-  }
-
-  componentDidMount() {
-    this._isMounted = true;
-    //Fetch Applications Recent
+  useEffect(() => {
+    let isMounted = true;
     axios.defaults.baseURL = "http://localhost:80";
     axios({
       method: "get",
       url: "/api/departmentAnnouncements/recent",
       headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
     }).then((res) => {
-      if (this._isMounted) {
-        this.setState({ recentAnnouncements: res.data });
-        console.log(this.state.recentAnnouncements);
+      if (isMounted) {
+        setRecentAnnouncements(res.data);
+        console.log(res.data);
       }
     });
-    console.log(this.state.recentAnnouncements);
-  }
 
-  componentWillUnmount() {
-    this._isMounted = false;
-  }
-
-  render() {
-    const theme = {
-      calendarIcon: {
-        textColor: "white", // text color of the header and footer
-        primaryColor: "#0da472", // background of the header and footer
-        backgroundColor: "#fafafa",
-      },
+    return () => {
+      isMounted = false;
     };
+  }, []);
 
-    const days = [
-      "Monday",
-      "Tuesday",
-      "Wendesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-      "Sunday",
-    ];
-    const monthNames = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ];
+  const theme = {
+    calendarIcon: {
+      textColor: "white", // text color of the header and footer
+      primaryColor: "#0da472", // background of the header and footer
+      backgroundColor: "#fafafa",
+    },
+  };
 
-    return (
-      <div className="card">
-        <div className="mt-1" style={{ textAlign: "center" }}></div>
-        <ul>
-          {this.state.recentAnnouncements.map((announcement) => (
-            <li
-              style={{ listStyle: "none" }}
-              key={announcement.id}
-              className="mb-2 mt-1"
-            >
-              <div className="float-left mr-2">
-                <time dateTime="2014-09-20" className="icon p-0">
-                  <em>{days[new Date(announcement.createdAt).getDay()]}</em>
-                  <strong>
-                    {monthNames[new Date(announcement.createdAt).getMonth()]}
-                  </strong>
-                  <span>{new Date(announcement.createdAt).getDate()}</span>
-                </time>
-              </div>
-              <span>
-                <strong>{announcement.announcementTitle}</strong> (
-                {announcement.department.departmentName})
-              </span>
-              <br className="p-1" />
-              <small>{announcement.announcementDescription}</small>
-              <hr className=" pt-2 pb-1 mb-0" />
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
-}
+  const days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  return (
+    <div className="card">
+      <div className="mt-1" style={{ textAlign: "center" }}></div>
+      <ul>
+        {recentAnnouncements.map((announcement) => (
+          <li
+            style={{ listStyle: "none" }}
+            key={announcement.id}
+            className="mb-2 mt-1"
+          >
+            <div className="float-left mr-2">
+              <time dateTime="2014-09-20" className="icon p-0">
+                <em>{days[new Date(announcement.createdAt).getDay()]}</em>
+                <strong>
+                  {monthNames[new Date(announcement.createdAt).getMonth()]}
+                </strong>
+                <span>{new Date(announcement.createdAt).getDate()}</span>
+              </time>
+            </div>
+            <span>
+              <strong>{announcement.announcementTitle}</strong>
+              {announcement.department && (
+                <span> ({announcement.department.departmentName})</span>
+              )}{" "}
+            </span>
+            <br className="p-1" />
+            <small>{announcement.announcementDescription}</small>
+            <hr className=" pt-2 pb-1 mb-0" />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default RecentAnnouncements;
