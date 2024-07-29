660972585ad4f887dbc638cfbe780d51ba76c421
ShravanKumarD <97720033+ShravanKumarD@users.noreply.github.com>
2024-07-29 11:23:13 +0530

    cors config

--- app.js
+++ app.js
@@ -27,8 +27,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors());
-app.use(cors({ origin: 'http://hrmssamcint.s3-website.ap-south-1.amazonaws.com' }));
+app.use(cors({ origin: '*' }));
 
 db.sequelize.sync({alter:true});
 
