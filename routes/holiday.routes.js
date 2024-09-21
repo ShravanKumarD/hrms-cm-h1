e89ba1ad0f4dca1be82f927710d0278d7e43c615
ShravanKumarD <97720033+ShravanKumarD@users.noreply.github.com>
2024-09-21 18:31:32 +0530

    added authenticTION FOR HOILIDAYS

--- routes/holiday.routes.js
+++ routes/holiday.routes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const holidayController = require('../controllers/holiday.controller');
 const withAuth = require('../withAuth')
 
-router.post('/',  withAuth.verifyToken,holidayController.createHoliday);
-router.get('/',  withAuth.verifyToken,holidayController.getAllHolidays);
-router.get('/:id', withAuth.verifyToken, holidayController.getHolidayById);
-router.put('/:id', withAuth.verifyToken, holidayController.updateHoliday);
-router.delete('/:id',  withAuth.verifyToken,holidayController.deleteHoliday);
+router.post('/',  withAuth.verifyToken,withAnyRole,holidayController.createHoliday);
+router.get('/',  withAuth.verifyToken,withAnyRole,holidayController.getAllHolidays);
+router.get('/:id', withAuth.verifyToken, withAnyRole,holidayController.getHolidayById);
+router.put('/:id', withAuth.verifyToken, withAnyRole,holidayController.updateHoliday);
+router.delete('/:id',  withAuth.verifyToken,withAnyRole,holidayController.deleteHoliday);
 
 module.exports = router;
