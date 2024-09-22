d3a52c31775e5a3511a6cb771a46c3e95703862b
ShravanKumarD <97720033+ShravanKumarD@users.noreply.github.com>
2024-09-22 17:31:18 +0530

    added cors policy

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: "https://hrms.creditmitra.in"}));
 
 db.sequelize.sync({ }).then(() => {
   console.log("Database synchronized.");
